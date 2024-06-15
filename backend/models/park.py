59a6a15d2ccb1ed2f3ded1feaaf8024f67949b99
imoleFolio <dremkay71@gmail.com>
2024-06-15 12:18:52 +0100

    restructuring

--- backend/models/park.py
+++ backend/models/park.py
@@ -1,6 +1,6 @@
 from models.base_model import Base, BaseModel
 from sqlalchemy import String, Column, ForeignKey
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 
 
 class Park(BaseModel,Base):
@@ -8,16 +8,16 @@ class Park(BaseModel,Base):
     __tablename__ = "parks"
 
     state = Column('state', String(100))
-    city = Column('city', String(100))
-    area = Column('area', String(100))
+    lga = Column('lga', String(100))
+    town = Column('town', String(100))
     address = Column('address', String(200))
     company_id = Column('company_id', ForeignKey('companies.id'))
     # routes = relationship('Route', backref='park')
 
-    def __init__(self, name: str, state: str, city: str, area: str, address: str, company_id: str) -> None:
+    def __init__(self, name: str, state: str, lga: str, town: str, address: str, company_id: str) -> None:
         super().__init__(name)
         self.state = state
-        self.city = city
-        self.area = area
+        self.lga = lga
+        self.town = town
         self.address = address
         self.company_id = company_id
--- backend/models/route.py
+++ backend/models/route.py
@@ -14,20 +14,20 @@ class Route(BaseModel, Base):
     company_id = Column('company_id', ForeignKey('companies.id'))
     
     from_state = Column('from_state', String(100))
-    from_city = Column('from_city', String(100))
-    from_area = Column('from_area', String(100))
+    from_lga = Column('from_lga', String(100))
+    from_town = Column('from_town', String(100))
 
     to_state = Column('to_state', String(100))
-    to_city = Column('to_city', String(100))
-    to_area = Column('to_area', String(100))
+    to_lga = Column('to_lga', String(100))
+    to_town = Column('to_town', String(100))
 
     from_park = relationship('Park', foreign_keys=[from_park_id], backref='routes_from')
     to_park = relationship('Park', foreign_keys=[to_park_id], backref='to_routes')
     
     def __init__(
             self, name: str, from_park_id: str, to_park_id: str, price: int,
-            time: str, company_id: str, from_state: str, from_city: str, from_area: str,
-            to_state: str, to_city: str, to_area: str
+            time: str, company_id: str, from_state: str, from_lga: str, from_town: str,
+            to_state: str, to_lga: str, to_town: str
             ) -> None:
         super().__init__(name)
         self.from_park_id = from_park_id
@@ -37,9 +37,9 @@ class Route(BaseModel, Base):
         self.company_id = company_id
 
         self.from_state = from_state
-        self.from_city = from_city
-        self.from_area = from_area
+        self.from_lga = from_lga
+        self.from_town = from_town
 
         self.to_state = to_state
-        self.to_city = to_city
-        self.to_area = to_area
+        self.to_lga = to_lga
+        self.to_town = to_town
