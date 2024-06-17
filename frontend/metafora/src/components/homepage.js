b2b0b674649d38471ed43e710864adc7fb6a44b2
Esseh12 <gbenmoese788@gmail.com>
2024-06-17 09:34:24 +0100

    first page done

--- frontend/metafora/src/components/homepage.js
+++ frontend/metafora/src/components/homepage.js
@@ -1,10 +1,19 @@
 import logo from '../images/logo-icon.ico';
-import React from 'react-dom';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Modal from './modal';
 import '../styles/homepage.css';
 
 const Homepage = () => {
+    const heading = ['Efficient', 'Reliable', 'Simplified'];
+
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    useEffect(() => {const intervalId = setInterval(() => {
+            setCurrentTextIndex(prevIndex => (prevIndex + 1) % heading.length);
+        }, 2500); // Change text every 2 seconds
+
+        return () => clearInterval(intervalId); // Cleanup interval on component unmount
+    }, [heading.length])
     const [modalOpen, setModalOpen] = useState(false);
     const handleOpenModal = () => {
         setModalOpen(true);
@@ -24,7 +33,7 @@ const Homepage = () => {
                             <div className="navbar-text">
                                 <ul className="navbar-list">
                                         <li className="navbar-item">Home</li>
-                                    <li className="navbar-item">About Us</li>
+                                    <li className="navbar-item">About</li>
                                     <li className="navbar-item">Services</li>
                                     <li className="navbar-item">Companies</li>
                                 <button className="navbar-button" onClick={handleOpenModal}>Login</button>
@@ -35,12 +44,26 @@ const Homepage = () => {
             </section>
             <Modal show={modalOpen} handleClose={handleCloseModal} />
             <section>
-                <div>
-                    <div class="">
-                        <div class="heroe-image">
-                            <div class="heroe-overlay"></div>
+                <div className="hero-container">
+                    <div className="heroe-image">
+                        <div className="heroe-overlay">
+                            <div className="hero-text">
+                                <h1>Discover {heading[currentTextIndex]} Transportation solution</h1>
+                            </div>
+                            <div className="main-booking-container">
+                                <div className="booking-container">
+                                    <div className="booking-items"><input placeholder='Leaving from' type="text"/></div>
+                                    <div className="booking-items"><input placeholder='Going to' type="text"/></div>
+                                    <div className="booking-items"><input type="month" placeholder='Leaving On'/></div>
+                                    <div className="booking-items"><p>Return(optional)</p></div>
+                                    <div><button>Search</button></div>
+                                    <div className="booking-container-overlay">
+                                        <h1>hello</h1>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        </div>
                 </div>
             </section>
         </>
--- frontend/metafora/src/components/modal.js
+++ frontend/metafora/src/components/modal.js
@@ -1,18 +1,21 @@
 import '../styles/modal.css'
-
 export const Modal = ({ show, handleClose}) => {
     return (
         <>
             <div className={`modal ${show ? 'show' : ''}`}>
                 <div className="modal-content">
                     <span className="close" onClick={handleClose}>&times;</span>
-                <h2 className="modal-heading">Welcome back</h2>
+                
+                <h2 className="modal-heading">Welcome Back</h2>
                 <form>
-                    <label htmlFor="username" class="modal-label">Email or Phone Number</label>
+                    <label htmlFor="username" class="modal-label">Email</label>
                     <input type="text" id="username" name="username" className="modal-input" required/>
                     <label htmlFor="password" class="modal-label" >Password</label>
-                    <input type="password" id="password" name="password" className="modal-input" required />
-                    <button type="submit" className="modal-button">Login</button>
+                        <input type="password" id="password" name="password" className="modal-input" required />
+                        <a href="#forgot" className="password-recovery link">Forgot password</a>
+                        <button type="submit" className="modal-button">Login</button>
+                        <p className="modal-text">Don't have an account&nbsp;?</p>
+                        <a href="#signup" className="signup link">sign up</a>
                 </form>
             </div>
             </div>
