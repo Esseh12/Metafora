c04e48f09160cae4daf30943533b17b677aae6be
imoleFolio <dremkay71@gmail.com>
2024-06-21 16:49:25 +0100

    Auhors updated

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,11 +7,10 @@ from models.base_model import Base
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db3.sqlite"
 
-app.secret_key = 'test_secret'
+app.secret_key = 'test_secret'   # should coming from enviroment variable
 
 db = SQLAlchemy(app, model_class=Base)
 
 
 with app.app_context():
     db.create_all()
-
