9fbc394c2ddaa666b46e53792d9c52e0c91e4f68
imoleFolio <dremkay71@gmail.com>
2024-06-27 15:48:32 +0100

    gitignored

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,6 +4,8 @@ from backend.models.base_model import Base
 from flask_migrate import Migrate
 from os import environ
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db4.sqlite"
@@ -14,7 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
 
 app.secret_key = environ.get('JWT_SECRET_KEY')   # needed for session or jwt to work
 
-
+CORS(app)
 db = SQLAlchemy(app, model_class=Base)
 jwt = JWTManager(app)
 
