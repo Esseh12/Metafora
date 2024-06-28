85378e6ad6c1e3f21e772930da946c6a6ec28246
Esseh12 <gbenmoese788@gmail.com>
2024-06-28 12:01:31 +0100

    finished the styling and responsiveness of login page

--- frontend/metafora/src/components/Authethication/login.js
+++ frontend/metafora/src/components/Authethication/login.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../styles/login.css';
-import backgroundImage from '../../images/AutheticationBackgroundImage.png';
+import Logo from '../../images/logo.png';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false); // state to toggle password visibility
     const navigate = useNavigate();
 
     const handleLogin = async (e) => {
@@ -13,54 +15,64 @@ const LoginPage = () => {
 
         // Dummy authentication logic
         if (email === 'user@example.com' && password === 'password') {
-            navigate('/'); // Navigate to the home page if authenticated
+            navigate('/');// Navigate to the home page if authenticated
         } else {
             alert('User not found. Please create an account.');
         }
     };
 
     return (
-        <div className="login-page" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="login-page" >
             <div className="login-container">
                 <div className="loginImg">
                     <img 
-                    src="/logo192.png" // Replace with your logo path
+                    src={Logo}
                     alt="Metafora Logo" 
                     className="logo"
                     onClick={() => navigate('/')}
                     />
+                    <h2 onClick={() => navigate('/')}>Metafora</h2>
                 </div>
                 <div className="login-subcontainer">
                     <div>
                         <h2>Jump right back in</h2>
-                        <p>Sign in to continue</p>
+                        <h5>Sign in to continue</h5>
                     </div>
                     <div className="login-form">
                         <form onSubmit={handleLogin}>
                             <label>
-                            Email Address
+                                Email Address
+                            </label>
                             <input 
                             type="email" 
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            required 
+                            required
                             />
-                            </label>
                             <label>
-                            Password
-                            <input 
-                            type="password" 
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required 
-                            />
+                                Password
                             </label>
-                            <button type="submit">Sign In</button>
+                            
+                            <div className="password-container">
+                                <input className='password-input'
+                                    type={showPassword ? "text" : "password"} 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required 
+                                />
+                                <span 
+                                    className="toggle-password" 
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </span>
+                            </div>
+                            <a href="/forgot-password" className='links'> Forgot password?</a>
+                            <div className="last__div">
+                                <a href="/signup"><span className="links__span">New User?</span>Create account</a>
+                                <button type="submit">Sign In</button>
+                            </div>
                         </form>
-                    <div className="links">
-                        <a href="/forgot-password">Forgot password?</a>
-                        <a href="/signup">Create account</a>
-                    </div>
                 </div>
                 </div>
             </div>
