c8eada3dabbf66e8f6101df3e85fa8a43134adcb
Esseh12 <gbenmoese788@gmail.com>
2024-07-02 14:45:33 +0100

    work on landing page

--- frontend/metafora/src/components/HomePage/testimonies.js
+++ frontend/metafora/src/components/HomePage/testimonies.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../../styles/testimonies.css';
+import oluchiDavies from "../../images/oluchiDavies.jpeg";
+import sharonIzuchukwu from "../../images/sharonIzuchukwu.webp";
+import adamaGarba from "../../images/adamuGarba.webp";
+import danielOmori from "../../images/danielOmori.jpeg";
 
 const Testimonies = () => {
     const testimonies = [
-        { id: 1, type: "customer", name: "Jane Doe", text: "Metafora made my journey so smooth and hassle-free. Highly recommended!" },
-        { id: 2, type: "customer", name: "John Smith", text: "I love the seamless booking process on Metafora. It's so easy and convenient." },
-        { id: 3, type: "customer", name: "Emily Johnson", text: "Finding reliable transportation has never been easier. Thanks, Metafora!" },
-        { id: 4, type: "partner", name: "Michael Brown", text: "Partnering with Metafora has boosted my business tremendously. Great platform!" },
-        { id: 5, type: "partner", name: "Sarah Wilson", text: "The best platform for transportation partners. Efficient and effective." },
-        { id: 6, type: "partner", name: "David Lee", text: "Metafora has connected me with so many clients. Couldn't be happier!" },
+        { id: 1, type: "customer", name: "Oluchi Davies", text: "Metafora made my journey so smooth and hassle-free. Highly recommended!", image: oluchiDavies },
+        { id: 2, type: "customer", name: "Jide Badmus", text: "I love the seamless booking process on Metafora. It's so easy and convenient.", image: adamaGarba },
+        { id: 3, type: "partner", name: "Sharon Ihemadu", text: "Partnering with Metafora has boosted my business tremendously.", image: sharonIzuchukwu },
+        { id: 4, type: "partner", name: "Daniel Omori", text: "The best platform for transportation partners. Efficient and effective.", image: danielOmori },
     ];
 
     return (
@@ -17,6 +19,7 @@ const Testimonies = () => {
             <div className="testimonies-container">
                 {testimonies.map(testimony => (
                     <div key={testimony.id} className="testimony-box">
+                        <img src={testimony.image} alt={testimony.name} className="testimony-image" />
                         <p className="testimony-text">"{testimony.text}"</p>
                         <p className="testimony-author">- {testimony.name}</p>
                         <span className={`testimony-type ${testimony.type}`}>{testimony.type}</span>
